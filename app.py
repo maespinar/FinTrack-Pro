f001a10376374312a67b667986156659eb4c1cad
Espinar <matiasespinar06@gmail.com>
2026-05-23 22:27:44 -0500

    feat: option 4

--- app.py
+++ app.py
@@ -15,28 +15,29 @@ def iniciar_sistema():
     portafolio = PortafolioAuditoria()
     while True:
         imprimir_cabecera()
-        opcion = input("\nSeleccione una opcion (1-5): ").strip()    
+        opcion = input("\nSeleccione una opción (1-5): ").strip()    
         match opcion:
             case "1":
-                print("\n[+] Modulo de registro iniciado (En construccion...)")
+                print("\n[+] Módulo de registro iniciado (En construcción...)")
                 try:
-                    ticker = input("Codigo de Ticker (Ej: AAPL): ").strip()
+                    ticker = input("Código de Ticker (Ej: AAPL): ").strip()
                     nombre = input("Nombre de la empresa: ").strip().title()
                     monto = float(input("Monto invertido original: ").strip())
                     divisa = str(input("Divisa de origen (Ej: EUR): ").strip())
                     tasa = float(input("Tasa de cambio a USD: ").strip())
                     nuevo_activo = ActivoInternacional(ticker, nombre, monto, divisa, tasa)
                     portafolio.añadir_activo(nuevo_activo)
-                    print(f"\n[✓] Exito! El activo {nuevo_activo.codigo_ticker} ha sido auditado y registrado.")
+                    print(f"\n[✓] Éxito! El activo {nuevo_activo.codigo_ticker} ha sido auditado y registrado.")
                     
                 except ValueError as e:
-                    print(f"\n[!] Error de valor: Ingrese valores numerios positivos validos.")
+                    print(f"\n[!] Error de valor: Ingrese valores numerios positivos válidos.")
                     print(f"Detalle de error: {e}")
                 except TickerDuplicadoError as e:
                     print(f"\n[!] Error de registro: {e}")
             case "2":
                 print("\n[+] Generando reporte del portafolio (En construcción...)")
-                portafolio.mostrar_reporte(portafolio.activos_dict)
+                lista_limpia = [{k: v for k, v in d.items() if k != "Consolidado_Num"} for d in portafolio.activos_dict]
+                portafolio.mostrar_reporte(lista_limpia)
             case "3":
                 print("\n[+] Iniciando filtro por divisa (En construcción...)")
                 try:
@@ -46,12 +47,12 @@ def iniciar_sistema():
                     print("\n[!] Error al ingresar la divisa.")
             case "4":
                 print("\n[+] Calculando consolidado en USD (En construcción...)")
-                
+                portafolio.calcular_valor_total_usd()
             case "5":
                 print("\n[!] Cerrando sesión segura de auditoría. ¡Hasta pronto!")
                 sys.exit()
             case _:
-                print("\n[x] Error de entrada: Por favor, seleccione una opcion valida del 1 al 5.")
+                print("\n[x] Error de entrada: Por favor, seleccione una opción valida del 1 al 5.")
     
 if(__name__ == "__main__"):
     iniciar_sistema()
--- finanzas/portafolio.py
+++ finanzas/portafolio.py
@@ -4,9 +4,9 @@ from tabulate import tabulate
 class PortafolioAuditoria:
     def __init__(self):
         self.activos_dict = [
-            {"Ticker": "AAPL", "Empresa": "Apple", "Inversión": "$1,500.00", "Divisa": "USD"},
-            {"Ticker": "AMZN", "Empresa": "Amazon", "Inversión": "€850.00", "Divisa": "EUR"},
-            {"Ticker": "SONY", "Empresa": "Sony Corp.", "Inversión": "¥150,000", "Divisa": "JPY"}
+            {"Ticker": "AAPL", "Empresa": "Apple", "Inversión": "$1,500.00", "Divisa": "USD", "Tasa USD": 1.0, "Consolidado_Num": 1500.00},
+            {"Ticker": "AMZN", "Empresa": "Amazon", "Inversión": "€850.00", "Divisa": "EUR", "Tasa USD": 1.09, "Consolidado_Num": 926.50},
+            {"Ticker": "SONY", "Empresa": "Sony Corp.", "Inversión": "¥150,000", "Divisa": "JPY", "Tasa USD": 0.0067, "Consolidado_Num": 1005.00}
         ]
         self.cantidad_activos =  3
     
@@ -14,11 +14,14 @@ class PortafolioAuditoria:
         for i in range(self.cantidad_activos):
             if activo.codigo_ticker == self.activos_dict[i]["Ticker"]:
                 raise TickerDuplicadoError(f"El activo con ticker {activo.codigo_ticker} ya esta registrado.")
+        valor_usd = activo.monto_invertido*activo.tasa_cambio_usd
         nuevo_dict_activo = {
             "Ticker": activo.codigo_ticker,
             "Empresa": activo.nombre,
             "Inversión": f"${activo.monto_invertido:,.2f}",
-            "Divisa": activo.divisa
+            "Divisa": activo.divisa,
+            "Tasa USD": activo.tasa_cambio_usd, 
+            "Consolidado_Num": valor_usd
         }
         self.activos_dict.append(nuevo_dict_activo)
         self.cantidad_activos += 1
@@ -27,11 +30,20 @@ class PortafolioAuditoria:
         lista_activos = []
         for activo in self.activos_dict:
             if activo["Divisa"] == divisa:
-                lista_activos.append(activo)
+                activo_visual = {k: v for k, v in activo.items() if k != "Consolidado_Num"}
+                lista_activos.append(activo_visual)
         self.mostrar_reporte(lista_activos) 
     
-    def calcular_valor_totalUSD(self):
-        return 0
+    def calcular_valor_total_usd(self):
+        lista_consolidada = []
+        for activo_dict in self.activos_dict:
+            activo_consolidado = {
+                "Ticker": activo_dict["Ticker"],
+                "Empresa": activo_dict["Empresa"],
+                "Consolidado USD": f"${activo_dict["Consolidado_Num"]:,.2f}"
+            }
+            lista_consolidada.append(activo_consolidado)
+        self.mostrar_reporte(lista_consolidada)
     
     def mostrar_reporte(self, lista_a_mostrar):
         if self.cantidad_activos == 0:
